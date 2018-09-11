0aee83b0782ecd9b3f3a258784d5302c7fc1ffd3
XeryuzRouge <alienslocos2000_@hotmail.com>
2018-09-11 16:30:50 -0500

    Players signs are now declared in Game class

--- game.rb
+++ game.rb
@@ -2,19 +2,19 @@ require_relative 'match_manager'
 
 class Game
 
-  attr_reader :players
+  attr_reader :players_config
 
   def initialize(player_config)
   	@output_interface = player_config.output_interface
     @input_interface = player_config.input_interface
-    p1 = player_config.player_request(1)
-    p2 = player_config.player_request(2)
-    @players = [p1, p2]
+    p1 = { sign: "X", config: player_config.player_request(1)}
+    p2 = { sign: "O", config: player_config.player_request(2)}
+    @players_config = { player1: p1, player2: p2}
     new_match
   end
 
   def new_match
-  	match_manager = MatchManager.new(output_interface, input_interface, players)
+  	 match_manager = MatchManager.new(output_interface, input_interface, players_config)
   end
 
   private
