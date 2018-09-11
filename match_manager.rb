8887ec50d20ed084655a424dd5f00ef4018d4d26
XeryuzRouge <alienslocos2000_@hotmail.com>
2018-09-11 16:07:16 -0500

    WinnerChecker now belongs to Board class, PlayerInput changed to HumanInput

--- match_manager.rb
+++ match_manager.rb
@@ -1,18 +1,17 @@
 require_relative 'board'
 require_relative 'board_display'
 require_relative 'turns_logic'
-require_relative 'player_input'
-require_relative 'winner_checker'
+require_relative 'human_input'
 require_relative 'cpu'
 
 class MatchManager
-	def initialize(output_interface, input_interface, players)
-		@turns = TurnsLogic.new(["X", "O"], players)
+	
+	def initialize(output_interface, input_interface, players_config)
 		@board = Board.new
-		@board_displayer = BoardDisplay.new(output_interface)
-		@winner_checker = WinnerChecker.new(board)
-		@player_input = PlayerInput.new(input_interface, board)
-		@cpu = CPU.new(board)
+		@turns = TurnsLogic.new(players_config)
+		@output = output_interface
+		@board_displayer = BoardDisplay.new(@output)
+		@human_input = HumanInput.new(input_interface, board)
 		@winner = false
 		display_board
 		start_match
@@ -20,44 +19,49 @@ class MatchManager
 
 	private
 
+	attr_reader :board
+	attr_reader :board_displayer
+	attr_reader :human_input
+	attr_reader :turns
+	attr_reader :winner
+	attr_reader :output
+
 	def start_match
     until (turns.completed ) || (winner == true) do
 			next_move
 		end
+		return turns.current_player if winner == true
+		return "" if winner == false
 	end
 
 	def next_move
 		turns.next
 		human_move_filter if turns.human_turn == true
-		cpu.move(turns.current_player) if turns.cpu_turn == true
+		if turns.cpu_turn == true
+			cpu = CPU.new(board, turns.current_player)
+			cpu.move(turns.current_turn)
+		end
 		display_board
-    got_winner if winner_checker.check_for_winner(turns.current_player) == 
-    turns.current_player
+    got_winner if board.check_for_winner(turns.current_player) == turns.current_player
 	end
 
 	def human_move_filter
 		move_accepted = false
 		until move_accepted == true do
-			move_accepted = player_input.play(turns.current_player)
-			print "\n invalid box\n" if move_accepted == false
+			move_accepted = human_input.play(turns.current_player)
+			@output.send "\ninvalid box\n" if move_accepted == false
 		end
 	end
 
 	def display_board
 		board_displayer.clear_screen
 		board_displayer.request(board.boxes)
+		@output.send "\n"
 	end
 
 	def got_winner
-		print "The winner is: #{turns.current_player}"
+		@output.send "The winner is: #{turns.current_player}"
 		@winner = true
 	end
-
-	attr_reader :board
-	attr_reader :board_displayer
-	attr_reader :turns
-	attr_reader :player_input
-	attr_reader :winner_checker
-	attr_reader :winner
-	attr_reader :cpu
+	
 end
