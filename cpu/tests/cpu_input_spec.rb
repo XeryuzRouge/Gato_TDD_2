e3661bdb2b0ecebb62d7b7dc88ec4c97aaff0683
XeryuzRouge <alienslocos2000_@hotmail.com>
2018-09-18 13:16:00 -0500

    cpu is now officially unbeatable

--- cpu/tests/cpu_input_spec.rb
+++ cpu/tests/cpu_input_spec.rb
@@ -4,14 +4,15 @@ require_relative '..\..\tests\tdd_classes'
 
 RSpec.describe CPUInput do
 
-  a = Array.new(30)
+  matches = Array.new(5000)
 
-  a.each do
+  matches.each do
 
     it 'should never lose' do
       board = FakeBoard.new
       player1 = "X" 
       player2 = "O"
+      winner = " "
       plays_recorder = {}
 
       opponent_cpu = CPUInput.new(board)
@@ -19,19 +20,23 @@ RSpec.describe CPUInput do
 
       turns.each do |i|
         if (i%2) == 0
-        choice = opponent_cpu.move(player2, i)
+          choice = opponent_cpu.move(player2, i)
         else
-        choice = board.play_on_box(board.available_boxes.sample, player1)
+          choice = board.play_on_box(board.available_boxes.sample, player1)
         end
-       plays_recorder[i] = choice
+
+        plays_recorder[i] = choice
+
+        if board.check_for_winner(player1) == player1
+          winner = [board.check_for_winner(player1), plays_recorder]
+          break
+        elsif board.check_for_winner(player2) == player2
+          winner = board.check_for_winner(player2)
+          break
+        end
       end
-      if board.check_for_winner(player1) == player1
-        winner = [board.check_for_winner(player1), plays_recorder]
-      elsif board.check_for_winner(player2) == player2
-        winner = board.check_for_winner(player2)
-      else
-        winner = "Tie"
-      end
+
+      winner = "Tie" if winner == " "
 
       expect(winner).to eq("O") | eq("Tie")
     end
