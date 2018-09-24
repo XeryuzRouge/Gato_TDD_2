4a3e8eb9e381434e905bc67855fe046b385b088b
XeryuzRouge <alienslocos2000_@hotmail.com>
2018-09-24 15:57:24 -0500

    Output & input are now modules

--- game_configuration/player_configuration.rb
+++ game_configuration/player_configuration.rb
@@ -1,14 +1,18 @@
+
 require_relative 'game_configuration'
+require_relative 'output_module'
+require_relative 'input_module'
 
 class PlayerConfiguration
 
-  attr_reader :output_interface
-  attr_reader :input_interface
+  include InputModule
+  include OutputModule
+
   attr_reader :game_config
 
-  def initialize(output_interface, input_interface)
-    @output_interface = output_interface
-    @input_interface = input_interface
+  def initialize
+    init_input
+    init_output
     @game_config = GameConfiguration.new
   end
 
@@ -16,18 +20,18 @@ class PlayerConfiguration
     option_selected = " "
     sign = game_config.game_characters(player)
 
-    output_interface.clear_display
-    output_interface.send "player #{player}, human or computer? (h/c)"
+    clear_display
+    show "player #{player}, human or computer? (h/c)"
 
     loop do
-      option_selected = input_interface.receive
+      option_selected = receive
       game_config.players_characters.each do |i|
         choice = i[0]
         i[1].each do |j|
           return { player_type: choice, sign: sign} if j == option_selected
         end
       end
-      output_interface.send "h for human or c for computer.."
+      show "h for human or c for computer.."
     end
 
   end
--- game_configuration/tests/player_configuration_spec.rb
+++ game_configuration/tests/player_configuration_spec.rb
@@ -1,30 +1,35 @@
 
-require_relative '..\..\tests\tdd_classes'
-require_relative '..\player_configuration'
+require_relative '../../tests/tdd_classes'
+require_relative '../player_configuration'
 
 RSpec.describe PlayerConfiguration do   
 
-    let(:player_configuration) { PlayerConfiguration.new(output_interface, input_interface) }
+  let(:player_configuration) { PlayerConfiguration.new }
+  let(:output) { FakeOutput.new }
+  let(:input) { FakeInput.new }
 
-    let(:output_interface) { FakeOutputInterface.new }
-    let(:input_interface) { FakeInputInterface.new }
+  before(:each) do
+    player_configuration.init_output(output)
+    player_configuration.init_input(input)
+  end
 
-    it "Should receive player 1 as human" do
-      input_interface.input_manager.this_one("h")
-      expected_hash = {:player_type=>:human, :sign=>"X"}
-      expect(player_configuration.player_request(1)).to eq expected_hash
-    end
+  it "Should receive player 1 as human" do
+    input.this_one("h")
+    expected_hash = { :player_type=>:human, :sign=>"X" }
+    expect(player_configuration.player_request(1)).to eq expected_hash
+  end
 
-    it "Should receive player 2 as computer" do
-      input_interface.input_manager.this_one("C")
-      expect(player_configuration.player_request(2)).to eq expected_hash = {:player_type=>:computer, :sign=>"O"}
-    end
+  it "Should receive player 2 as computer" do
+    input.this_one("C")
+     expected_hash = { :player_type=>:computer, :sign=>"O" }
+    expect(player_configuration.player_request(2)).to eq expected_hash
+  end
 
-    it "should loop message error until it gets a valid character" do
-      input_interface.input_manager.try_these(["w", "1", "H"])
-      player_configuration.player_request
-      expect(output_interface.output_manager.messages[1]).to eq "h for human or c for computer.."
-      expect(output_interface.output_manager.messages[2]).to eq "h for human or c for computer.."
-    end
+  it "should loop message error until it gets a valid character" do
+    input.try_these(["w", "1", "H"])
+    player_configuration.player_request
+    expect(output.messages[1]).to eq "h for human or c for computer.."
+    expect(output.messages[2]).to eq "h for human or c for computer.."
+  end
 
 end
