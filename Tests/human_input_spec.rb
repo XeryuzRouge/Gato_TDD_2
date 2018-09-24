f3a0bb73bc109639e285b44c7d03b165b181d972
XeryuzRouge <alienslocos2000_@hotmail.com>
2018-09-24 15:35:55 -0500

    Input & output are now modules instead of arguments

--- Tests/human_input_spec.rb
+++ Tests/human_input_spec.rb
@@ -5,22 +5,27 @@ require_relative 'tdd_classes'
 RSpec.describe HumanInput do
 
   let(:board) { FakeBoard.new }
-  let(:output) { FakeOutputInterface.new }
-  let(:input) { FakeInputInterface.new }
-	let(:human_input) { HumanInput.new(output, input, board) }
+  let(:output) { FakeOutput.new }
+  let(:input) { FakeInput.new }
+	let(:human_input) { HumanInput.new(board) }
+
+  before(:each) do
+    human_input.init_output(output)
+    human_input.init_input(input)
+  end
 
 	it 'should let make a play' do
-    input.input_manager.this_one("2")
+    input.this_one("2")
     human_input.play("X")
     expect(board.player_boxes("X")).to eq ["2"]
 	end
 
   it 'should tell player that he is trying to play an invalid box' do
-    input.input_manager.this_one("2")
+    input.this_one("2")
     human_input.play("O")
     this_one = "2"
     then_this_one = "3"
-    input.input_manager.try_these([this_one, then_this_one])
+    input.try_these([this_one, then_this_one])
     human_input.play("X")
     expect(output.message).to eq "\ninvalid box\n"
   end
---
+++ human_input.rb
@@ -0,0 +1,34 @@
+
+require_relative 'game_configuration/output_module'
+require_relative 'game_configuration/input_module'
+
+class HumanInput
+
+  include OutputModule
+  include InputModule
+  
+  def initialize(board)
+    init_output
+    init_input
+    @board = board
+  end
+
+  def play(player)
+    choice = receive
+    input_filter(choice, player)
+  end
+
+  private
+
+  attr_reader :board
+
+  def input_filter(choice, player)
+    if board.available_boxes.include? choice
+      board.play_on_box(choice, player)
+    else
+      show "\ninvalid box\n"
+      play(player)
+    end
+  end
+  
+end
