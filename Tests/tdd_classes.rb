6eb70d7d4dfd01e8930e683100231d96f2b977a2
XeryuzRouge <alienslocos2000_@hotmail.com>
2018-09-25 12:47:54 -0500

    cpu_input's test is now failing

--- Tests/tdd_classes.rb
+++ Tests/tdd_classes.rb
@@ -42,3 +42,58 @@ class FakeOutput
   end
 
 end
+
+class FakePlaysCounter
+
+  def initialize
+    @corner_boxes = ["7", "9", "1", "3"]
+    @cross_boxes = ["4", "8", "2", "6"]
+  end
+
+  def special_moves(board, player, turn)
+    @board = board
+    @player = player
+    if turn <= 2
+      first_move
+    else
+      second_move(turn)
+    end
+  end
+
+  private
+
+  attr_reader :board
+  attr_reader :player
+  attr_reader :corner_boxes
+  attr_reader :cross_boxes
+
+  def first_move
+    if board.available_boxes.include? "5"
+      return "5"
+    else
+      return corner_boxes.sample
+    end
+  end
+
+  def second_move(turn)
+
+    return (corner_boxes & board.available_boxes).sample if turn == 3
+
+    opponent_boxes = board.opponent_boxes(player)
+    counter_plays.each do |i|
+      if (opponent_boxes.include? i[0]) && (opponent_boxes.include? i[1])
+        return (i[2] & board.available_boxes).sample
+      end
+    end
+    return nil
+  end
+
+  def counter_plays
+    [["7", "3", cross_boxes], ["1", "9", cross_boxes],
+    ["7", "5", corner_boxes], ["9", "5", corner_boxes], 
+    ["1", "5", corner_boxes], ["3", "5", corner_boxes],
+    ["3", "4", ["1"]], ["1", "6", ["3"]], ["4", "9", ["7"]], ["6", "7", ["9"]],
+    ["1", "8", ["7"]], ["3", "8", ["9"]], ["2", "7", ["1"]], ["2", "9", ["3"]]]
+  end
+
+end
--- cpu/cpu_input.rb
+++ cpu/cpu_input.rb
@@ -4,19 +4,21 @@ require_relative 'board_analyzer'
 
 class CPUInput
 
+   attr_accessor :plays_counter
+
   def initialize(board, thinking_time)
     @player = player
+    @plays_counter = PlaysCounter.new
     @board = board
     @thinking_time = thinking_time
   end
 
   def move(player, turn)
     board_analyzer = BoardAnalyzer.new
-    plays_counter = PlaysCounter.new
     box_selected = nil
 
     box_selected = board_analyzer.analyze(board, player)
-    box_selected = plays_counter.special_moves(board, player, turn) if box_selected == nil && turn <= 4
+    box_selected = @plays_counter.special_moves(board, player, turn) if box_selected == nil && turn <= 4
     box_selected = board.available_boxes.sample if box_selected == nil
     sleep(thinking_time)
     board.play_on_box(box_selected.to_s, player)
